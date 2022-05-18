47268f2aeeb336aeffbda322db78836aaa75305f
NguyenVanSang-coder <62025815+NguyenVanSang-coder@users.noreply.github.com>
2022-05-18 14:05:48 +0700

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-app.get("/", function (req, res) {
+app.get("/",  (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
